dff65b0d6bd0452375f6ecb4c1c0b1531e57e056
mariem <mariemzarrouk41@example.com>
2025-07-03 20:23:26 +0200

    Fusion de la branche projete2 dans main

--- projet.py
+++ projet.py
@@ -6,6 +6,15 @@ def somme(T):
     return S
 =======
 Data=[1,3,5]
+<<<<<<< HEAD
 Som=sum(Data)
 print('la somme est:', Som)
+>>>>>>> projete2
+=======
+if Data:
+    print('la somme est:', sum(Data))
+    print('le min est: ', min(Data))
+    print('le max est: ', max(Data))
+else:
+    print('dossier vide')
 >>>>>>> projete2
